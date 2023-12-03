f404d1684560fbefe9f551921ac5fa5314fb68a4
polybeandip <akashdhiraj2019@gmail.com>
2023-12-03 03:32:26 -0500

    day 3, both parts

--- 2023/three/Three.java
+++ 2023/three/Three.java
@@ -1,9 +1,11 @@
 import java.io.*;
+import java.util.*;
 
 class Three {
   private static char[][] map;
   private static int width = 0;
   private static int height = 1;
+  private static Map<List<Integer>, List<Integer>> gears = new HashMap<List<Integer>, List<Integer>>();
 
   public static void main(String[] args) throws IOException {
     String file = "three.txt";
@@ -34,25 +36,51 @@ class Three {
       StringBuilder num = new StringBuilder();
       boolean part = false;
       boolean inside = false;
+      Set<List<Integer>> symbols = new HashSet<List<Integer>>();
       for (int j = 0; j < width; j++) {
         char c = map[i][j];
         if ((c - '0') <= 9 && (c -'0') >= 0) {
           num.append(c);
-          part = part || is_part(i,j);
+          part = part || is_part(i,j, symbols);
           inside = true;
         }
         else if (inside) {
-          int b = Integer.valueOf(num.toString());
-          //System.out.println(b);
-          if (part) sum += b;
+          int n = Integer.valueOf(num.toString());
+          if (part) sum += n;
+          for (List<Integer> coord : symbols) {
+            if (!gears.containsKey(coord))
+              gears.put(coord, new ArrayList<Integer>());
+            gears.get(coord).add(n);
+          }
+
           inside = false;
           part = false;
           num = new StringBuilder();
+          symbols = new HashSet<List<Integer>>();
+        }
+      }
+
+      if (inside) {
+        int n = Integer.valueOf(num.toString());
+        if (part) sum += n;
+        for (List<Integer> coord : symbols) {
+          if (!gears.containsKey(coord))
+            gears.put(coord, new ArrayList<Integer>());
+          gears.get(coord).add(n);
         }
       }
     }
 
+    int ratio = 0;
+    for (Map.Entry<List<Integer>, List<Integer>> entry : gears.entrySet()) {
+      List<Integer> gear = entry.getValue();
+      if (gear.size() == 2) {
+        ratio += gear.get(0) * gear.get(1);
+      }
+    }
+
     System.out.println(sum);
+    System.out.println(ratio);
   }
   
   private static boolean is_symbol(int i, int j) {
@@ -62,13 +90,20 @@ class Three {
     return (ndist < 0 || ndist > 9) && map[i][j] != '.';
   }
 
-  private static boolean is_part(int i, int j) {
+  private static boolean is_part(int i, int j, Set<List<Integer>> symbols) {
+    boolean out = false;
+
     for (int dx = -1; dx < 2; dx++) {
       for (int dy = -1; dy < 2; dy++) {
         if (dx == 0 && dy == 0) continue;
-        if (is_symbol(i+dx, j + dy)) return true;
+        if (is_symbol(i+dx, j + dy)) {
+          out = true;
+          if (map[i+dx][j+dy] == '*')
+            symbols.add(Arrays.asList(i+dx, j + dy));
+        }
       }
     }
-    return false;
+
+    return out;
   }
 }
