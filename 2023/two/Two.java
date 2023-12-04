8fabefdad5e1c2f8082a2d2b3b66702212de0453
polybeandip <akashdhiraj2019@gmail.com>
2023-12-04 02:30:28 -0500

    spacing

--- 2023/two/Two.java
+++ 2023/two/Two.java
@@ -6,13 +6,15 @@ class Two {
   private static int blue = 14;
   public static void main(String[] args) throws IOException {
     String file = "two.txt";
-    BufferedReader scan = new BufferedReader(new FileReader(file));
+    if (args.length >= 1 && args[0].equals("-t")) file = "test.txt";
+    BufferedReader scan = new BufferedReader(new FileReader(file)); 
 
     int part1 = 0, part2 = 0;
     while (scan.ready()) {
       String line = scan.readLine();
       String[] split = line.split(":");
       int id = Integer.valueOf(split[0].substring(5));
+
       boolean valid =  true;
       int max_r = 0, max_b = 0, max_g = 0;
       for (String piece : split[1].split(";")) {
@@ -56,6 +58,7 @@ class Two {
     }
 
     scan.close();
+
     System.out.println(part1);
     System.out.println(part2);
   }
